0551bdfeb2925cd6f0f1691530ff3fb26332728a
Mathias Stadler <stadler-mathias@web.de>
2019-10-07 13:09:58 +0200

    save

--- create-debian-images.sh
+++ create-debian-images.sh
@@ -20,7 +20,7 @@ mkdir linux && cd $_
 ../repo/repo init --repo-url=https://github.com/rockchip-linux/repo -u https://github.com/rockchip-linux/manifests -b master -m rk3399_linux_release.xml
 
 # checkout all repo
-../repo/repo -c -jobs=20 sync
+../repo/repo sync --current-branch --jobs=20
 
 # create all images
 ./build.sh all
@@ -28,5 +28,24 @@ mkdir linux && cd $_
 # create 
 ./mkfirmware.sh buildroot
 
+# install rktool for 64 bit
+cd && cd develop
+git clone https://github.com/rockchip-android/RKTools.git
+
+# save old version
+mv ~/develop/linux/tools/linux/Linux_Pack_Firmware/rockdev/afptool ~/develop/linux/tools/linux/Linux_Pack_Firmware/rockdev/afptool_not64Bit
+mv ~/develop/linux/tools/linux/Linux_Pack_Firmware/rockdev/rkImageMaker ~/develop/linux/tools/linux/Linux_Pack_Firmware/rockdev/rkImageMaker_not64Bit
+
+# create link
+
+ln -s ~/develop/RKTools/linux/Linux_Pack_Firmware/rockdev/afptool ~/develop/linux/tools/linux/Linux_Pack_Firmware/rockdev/afptool
+ln -s ~/develop/RKTools/linux/Linux_Pack_Firmware/rockdev/rkImageMaker ~/develop/linux/tools/linux/Linux_Pack_Firmware/rockdev/rkImageMaker
+
+# create update.img
+~/develop/linux/./build.sh updateimg
+
+echo "finish "
+echo "you can flash the image to sbc with the command"
+echo "sudo ~/develop/linux/rkflash.sh update ~/develop/linux/rockdev/update.img"
 
 
